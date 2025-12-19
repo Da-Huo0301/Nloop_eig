8e8fb28640b5dbe3d11e6ef2c9cde46fdb9f96e0
Da-Huo0301 <dahu4353@student.su.se>
2025-12-19 21:51:14 +0100

    Add files via upload

---
+++ Modified code/Om极化率三维/N4loop_eit_num_phase_putongyuanzi_dpbian_sanwei.m
@@ -0,0 +1,134 @@
+% THE NUMERICAL RESULT OF THE LADDER-STYLE ELECTROMAGNETICALLY INDUCED
+% TRANSPARENCY.
+clear;clc;
+tic
+% danwei=2*pi*10
+G21 = 3*2*pi*1e6;
+G23 = 3*2*pi*1e6;
+G43 = 1*2*pi*1e3;
+g21=(G23+G21)/2;
+g31 = 0.0011*2*pi*1e3;
+g23=(G23+G21)/2;
+g24=(G23+G21+G43)/2;
+g43=G43/2;
+g41=G43/2;
+%Dc = 0.0;
+Dr =0*2*pi*1e6;
+Dc0=10*2*pi*1e6;
+Omr = (0:0.001:2)*2*pi*1e6;
+theta_p = 0;
+theta_c = 0;
+theta_m =3*pi/2;
+Dp = 0*2*pi*1e6;
+Op =0.01*2*pi*1e6;
+Oc = 1*2*pi*1e6;
+Od = 1*2*pi*1e6;
+
+Op1 = Op*exp(1i*theta_p); Op2 = Op*exp(-1i*theta_p);
+
+
+
+
+y = zeros(15,1);
+re = zeros('like',Omr);
+im = zeros('like',Omr);
+lambda=780*10^(-9);
+
+a=390*10^(-9);
+N0=1*1e10*1e6;
+%N0=5.5*1e11*1e6;% 原子密度;%%%%%%%%%%%%%%%%设多少合适？
+h=0.1*a;%%%%%%%%%%%%%%半高宽设多少合适？
+
+DD=3.58*10^(-29);
+e0=8.854188*10^(-12);
+Hbar=1.054572*10^(-34);
+yet=N0*DD^2/(2*e0*Hbar);
+   
+epslon=yet;
+
+cc=0;
+for x=-0.5*a:a/2000:0.5*a
+    cc=cc+1;
+Dc=Dc0*sin(2*pi*x/lambda);
+%DC(cc)=Dc;
+xx(cc)=x;
+index = 0;
+for Om = Omr
+     index = index + 1;  
+     OM(index)=Om;
+     Od1 = Od; Od2 = Od;
+   Oc1= Oc*exp(1i*theta_c); Oc2= Oc*exp(-1i*theta_c);
+    %N(ss)=N0*exp(-(z-z0)^2/h^2);% 光晶格-高斯分布
+%     Oc1=Oc0+Oc11*sin(2*pi*z/a);     
+%     Oc2=Oc1;     
+    % A and b are obtaied from "ladder_eit_sym.m". 
+   A =[               0,           0,                0,             -g21, -Om*sin(theta_m),       0,                  0,                0,                0,             -Dp,  Om*cos(theta_m),       0,               -Oc1,                0,               0;...
+               0,           0,                0,  Om*sin(theta_m),             -g23,       0,                  0,                0,                0, Om*cos(theta_m),              -Dc,     Od2,                Op1,                0,               0;...
+ Om*sin(theta_m),           0, -Om*sin(theta_m),                0,                0,       0,               -g31,                0,                0,            -Oc2,             -Op1,       0,            Dc - Dp,                0,            -Od1;...
+               0,           0,                0,                0,                0,    -g24,                  0,                0,                0,               0,              Od1, Dr - Dc,                  0,             -Oc1,             Op1;...
+               0,           0,                0,                0,                0,       0,                  0, -Om*sin(theta_m),             -g41,               0,                0,    -Op1,               -Od2, -Om*cos(theta_m),    Dc - Dp - Dr;...
+               0,           0,                0,                0,                0,       0,                  0,             -g43,  Om*sin(theta_m),               0,                0,    -Oc1,                  0,               Dr, Om*cos(theta_m);...
+               0,         G21,                0,                0,                0,       0, -2*Om*sin(theta_m),                0,                0,     - Op1 - Op2,                0,       0, -2*Om*cos(theta_m),                0,               0;...
+               0, - G21 - G23,                0,                0,                0,       0,                  0,                0,                0,       Op1 + Op2,        Oc1 + Oc2,       0,                  0,                0,               0;...
+            -G43,   G23 - G43,             -G43,                0,                0,       0,  2*Om*sin(theta_m),                0,                0,               0,      - Oc1 - Oc2,       0,  2*Om*cos(theta_m),        Od1 + Od2,               0;...
+             Op1,        -Op1,                0,               Dp, -Om*cos(theta_m),       0,                Oc1,                0,                0,            -g21, -Om*sin(theta_m),       0,                  0,                0,               0;...
+               0,        -Oc1,              Oc1, -Om*cos(theta_m),               Dc,    -Od2,                Op1,                0,                0, Om*sin(theta_m),             -g23,       0,                  0,                0,               0;...
+ Om*cos(theta_m),           0, -Om*cos(theta_m),              Oc2,             -Op1,       0,            Dp - Dc,                0,              Od1,               0,                0,       0,               -g31,                0,               0;...
+               0,           0,                0,                0,             -Od1, Dc - Dr,                  0,              Oc1,              Op1,               0,                0,    -g24,                  0,                0,               0;...
+               0,           0,                0,                0,                0,    -Op1,                Od2, -Om*cos(theta_m),     Dp - Dc + Dr,               0,                0,       0,                  0,  Om*sin(theta_m),            -g41;...
+             Od2,         Od2,            2*Od2,                0,                0,    -Oc1,                  0,               Dr, -Om*cos(theta_m),               0,                0,       0,                  0,              g43, Om*sin(theta_m)];
+ 
+    b =[0; 0; 0; 0; 0; 0; 0; 0;-G43; 0; 0; 0; 0; 0; Od2];
+    y = A\b;
+    rho_21 = (y(4) + 1i*y(10))*epslon;
+    re(cc,index) = real(rho_21)/Op;
+    im(cc,index) = imag(rho_21)/Op;   
+    
+    
+end 
+end
+ %save matfile1.mat xx re im OC;
+% save re;
+% save im;
+figure(1)
+mesh(Omr/(2*pi*1e6),xx/a,re);
+ylabel('x/a');
+xlabel('\Omega_m');
+zlabel('Re(\chi)');
+%set(gca,'FontSize',20,'Fontname', 'Times New Roman');
+hold on
+figure(2)
+mesh(Omr/(2*pi*1e6),xx/a,im);
+ylabel('x/a');
+xlabel('\Omega_m');
+zlabel('Im(\chi)');
+%set(gca,'FontSize',20,'Fontname', 'Times New Roman');
+hold on
+
+
+
+% figure(3)
+% R=6;M=10;lamdp=780*1e-9;
+%  L=1*1e-4;%这个是介质衍射方向长度，自己设定，越长增益越大
+% 
+% 
+% Ip=zeros(501,1001);
+% for index=1:1001   
+% for cc=1:2001
+% T1(cc,index)=exp(-2*pi*L*im(cc,index)/lamdp)*exp(1i*2*pi*L*re(cc,index)/lamdp);
+% end
+% for k=1:3
+% Ip(k,index)=diffraction_1D_2000_1(R,M,T1,index,k);
+% end
+% end
+% 
+% figure(1)
+% plot(OC/(2*pi*1e6),Ip(1,:),'r-',OC/(2*pi*1e6),Ip(2,:),'b-',OC/(2*pi*1e6),Ip(3,:),'k-');
+% hold on
+% 
+% 
+%  
+
+% toc
+
+
